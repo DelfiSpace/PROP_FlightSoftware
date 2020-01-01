af6c3ca718cc665a8ab9bedfc7189f548cc60fb8
Stefano Speretta <s.speretta@tudelft.nl>
2020-01-02 00:14:10 +0100

    Using PQ9Sender

--- TestService.h
+++ TestService.h
@@ -9,13 +9,12 @@
 #define TESTSERVICE_H_
 
 #include "Service.h"
-#include "PQ9Frame.h"
 #include "DSerial.h"
 #include <driverlib.h>
 
 class TestService: public Service
 {
  public:
-     virtual bool process( PQ9Frame &command, PQ9Bus &interface, PQ9Frame &workingBbuffer );
+     virtual bool process( PQ9Frame &command, PQ9Sender &interface, PQ9Frame &workingBbuffer );
 };
 #endif /* TESTSERVICE_H_ */
